6c37f97e3235e9dcd722ef59e9078c80630c0430
MetaMechanists <percy.baker.0@gmail.com>
2023-04-26 22:49:50 +0100

    -

--- src/ComponentTest/ComponentTests.cpp
+++ src/ComponentTest/ComponentTests.cpp
@@ -17,15 +17,18 @@ namespace ComponentTests {
 
             vector<ComponentTestCase> group_main;
             group_main.reserve(4095);
+
             for (int x = 2; x < 4096; x++) {
                 group_main.push_back(ComponentTestCase{ 
                     {{"Clk_write", TestNumber{0, false}}}, 
                 });
+
                  group_main.push_back(ComponentTestCase{ 
                     {{"Clk_write", TestNumber{1, false}}}, 
                     {{"Data_out", TestNumber{x, false}}}
                 });
             }
+
             test_groups.insert(make_pair("main", group_main));
 
             return ComponentTest(pin_blocks, test_groups);
@@ -41,29 +44,58 @@ namespace ComponentTests {
             pin_blocks.insert(make_pair("Test_out", GPIO::PinBlock{13, 8, INPUT}));
 
             vector<ComponentTestCase> group_one_register_all_values;
+
             for (int reg = 1; reg < 15; reg++) {
                 for (int val = -128; val < 128; val++) {
                     group_one_register_all_values.push_back(ComponentTestCase{ 
-                       {{"Clk", 0}, {"Select", reg}, {"Data_in", val}}, 
-                       {} });
+                        {
+                            {"Clk", TestNumber{0, false}}, 
+                            {"Select", TestNumber{reg, false}}, 
+                            {"Data_in", TestNumber{val, true}}
+                        }, 
+                        {} 
+                    });
+
                     group_one_register_all_values.push_back(ComponentTestCase{ 
-                        {{"Clk", 1}, {"Select", reg}, {"Data_in", val}}, 
-                        {{"Test_out", val}} });
+                        {
+                            {"Clk", TestNumber{1, false}}, 
+                            {"Select", TestNumber{reg, false}}, 
+                            {"Data_in", TestNumber{val, true}}
+                        }, 
+                        {
+                            {"Test_out", TestNumber{val, true}}
+                        } 
+                    });
                 }
             }
+
             test_groups.insert(make_pair("All registers one value", group_one_register_all_values));
 
             vector<ComponentTestCase> group_all_registers_one_value;
+
             for (int val = -128; val < 128; val++) {
                 for (int reg = 1; reg < 15; reg++) {
                     group_all_registers_one_value.push_back(ComponentTestCase{ 
-                       {{"Clk", 0}, {"Select", reg}, {"Data_in", val}}, 
-                       {} });
+                        {
+                            {"Clk", TestNumber{0, false}}, 
+                            {"Select", TestNumber{reg, false}}, 
+                            {"Data_in", TestNumber{val, true}}}, 
+                        {} 
+                    });
+
                     group_all_registers_one_value.push_back(ComponentTestCase{ 
-                        {{"Clk", 1}, {"Select", reg}, {"Data_in", val}}, 
-                        {{"Test_out", val}} });
+                        {
+                            {"Clk", TestNumber{1, false}}, 
+                            {"Select", TestNumber{reg, false}}, 
+                            {"Data_in", TestNumber{val, true}}
+                        }, 
+                        {
+                            {"Test_out", TestNumber{val, true}}
+                        } 
+                    });
                 }
             }
+
             test_groups.insert(make_pair("One register all values", group_all_registers_one_value));
 
             return ComponentTest(pin_blocks, test_groups);
@@ -98,11 +130,32 @@ namespace ComponentTests {
             vector<ComponentTestCase> group_test;
             for (int val = -128; val < 128; val++) {
                 group_test.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", val}, {"Data-2_in", 0}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}, {"Write_flags", 0}}, 
-                    {} });
+                    {
+                        {"Data-1_in", TestNumber{val, true}}, 
+                        {"Data-2_in", TestNumber{0, false}}, 
+                        {"Invert", TestNumber{0, false}}, 
+                        {"Increment", TestNumber{0, false}}, 
+                        {"Propagate", TestNumber{0, false}}, 
+                        {"Write_flags", TestNumber{0, false}}}, 
+                    {} 
+                });
+
                 group_test.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", val}, {"Data-2_in", 0}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}, {"Write_flags", 1}}, 
-                    {{"Greater", val > 0}, {"Less", val < 0}, {"Equal", val == 0}, {"NotEqual", val != 0}} });
+                    {
+                        {"Data-1_in", TestNumber{val, true}}, 
+                        {"Data-2_in", TestNumber{0, false}}, 
+                        {"Invert", TestNumber{0, false}}, 
+                        {"Increment", TestNumber{0, false}}, 
+                        {"Propagate", TestNumber{0, false}}, 
+                        {"Write_flags", TestNumber{1, false}}
+                    }, 
+                    {
+                        {"Greater", TestNumber{val > 0, false}}, 
+                        {"Less", TestNumber{val < 0, false}}, 
+                        {"Equal", TestNumber{val == 0, false}}, 
+                        {"NotEqual", TestNumber{val != 0, false}}
+                    }
+                });
             }
             test_groups.insert(make_pair("Test", group_test));
 
@@ -110,11 +163,32 @@ namespace ComponentTests {
             for (int a = -128; a < 128; a++) {
                 for (int b = -128; b < 128; b++) {
                     group_compare.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 1}, {"Increment", 1}, {"Propagate", 1}, {"Write_flags", 0}}, 
-                        {} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{1, false}}, 
+                            {"Increment", TestNumber{1, false}}, 
+                            {"Propagate", TestNumber{1, false}}, 
+                            {"Write_flags", TestNumber{0, false}}}, 
+                        {} 
+                    });
+
                     group_compare.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}, {"Write_flags", 1}}, 
-                        {{"Greater", a > b}, {"Less", a < b}, {"Equal", a == b}, {"NotEqual", a != b}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{0, false}}, 
+                            {"Increment", TestNumber{0, false}}, 
+                            {"Propagate", TestNumber{0, false}}, 
+                            {"Write_flags", TestNumber{1, false}}
+                        }, 
+                        {
+                            {"Greater", TestNumber{a > b, false}}, 
+                            {"Less", TestNumber{a < b, false}}, 
+                            {"Equal", TestNumber{a == b, false}}, 
+                            {"NotEqual", TestNumber{a != b, false}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("Compare", group_compare));
@@ -149,8 +223,18 @@ namespace ComponentTests {
                     }
 
                     group_add.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 1}}, 
-                        {{"Carry", carry}, {"Data_out", result}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{0, false}}, 
+                            {"Increment", TestNumber{0, false}}, 
+                            {"Propagate", TestNumber{1, false}}
+                        }, 
+                        {
+                            {"Carry", TestNumber{carry, false}}, 
+                            {"Data_out", TestNumber{result, true}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("Add", group_add));
@@ -168,8 +252,18 @@ namespace ComponentTests {
                     }
 
                     group_sub.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 1}, {"Increment", 1}, {"Propagate", 1}},
-                        {{"Carry", carry}, {"Data_out", result}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{1, false}}, 
+                            {"Increment", TestNumber{1, false}}, 
+                            {"Propagate", TestNumber{1, false}}
+                        },
+                        {
+                            {"Carry", TestNumber{carry, false}}, 
+                            {"Data_out", TestNumber{result, true}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("Subtract", group_sub));
@@ -177,32 +271,70 @@ namespace ComponentTests {
             vector<ComponentTestCase> group_inc;
             for (int a = -128; a < 127; a++) {
                 group_inc.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", a}, {"Data-2_in", 0}, {"Invert", 0}, {"Increment", 1}, {"Propagate", 1}},
-                    {{"Carry", 0}, {"Data_out", a+1}} });
+                    {
+                        {"Data-1_in", TestNumber{a, true}},
+                        {"Data-2_in", TestNumber{0, false}}, 
+                        {"Invert", TestNumber{0, false}}, 
+                        {"Increment", TestNumber{1, false}}, 
+                        {"Propagate", TestNumber{1, false}}
+                    },
+                    {
+                        {"Carry", TestNumber{0, false}}, 
+                        {"Data_out", TestNumber{a+1, true}}
+                    } 
+                });
             }
             test_groups.insert(make_pair("Increment", group_inc));
 
             vector<ComponentTestCase> group_dec;
             for (int a = -127; a < 128; a++) {
                 group_dec.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", a}, {"Data-2_in", 0}, {"Invert", 1}, {"Increment", 0}, {"Propagate", 1}},
-                    {{"Carry", 0}, {"Data_out", a-1}} });
+                    {
+                        {"Data-1_in", TestNumber{a, true}}, 
+                        {"Data-2_in", TestNumber{0, false}}, 
+                        {"Invert", TestNumber{1, false}}, 
+                        {"Increment", TestNumber{0, false}}, 
+                        {"Propagate", TestNumber{1, false}}
+                    },
+                    {
+                        {"Carry", TestNumber{0, false}}, 
+                        {"Data_out", TestNumber{a-1, true}}
+                    } 
+                });
             }
             test_groups.insert(make_pair("Decrement", group_dec));
 
             vector<ComponentTestCase> group_inv;
             for (int b = -127; b < 128; b++) {
                 group_inv.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", 0}, {"Data-2_in", b}, {"Invert", 1}, {"Increment", 1}, {"Propagate", 1}},
-                    {{"Data_out", -b}} });
+                    {
+                        {"Data-1_in", TestNumber{0, false}}, 
+                        {"Data-2_in", TestNumber{b, true}}, 
+                        {"Invert", TestNumber{1, false}}, 
+                        {"Increment", TestNumber{1, false}}, 
+                        {"Propagate", TestNumber{1, false}}
+                    },
+                    {
+                        {"Data_out", TestNumber{-b, true}}
+                    } 
+                });
             }
             test_groups.insert(make_pair("Invert", group_inv));
 
             vector<ComponentTestCase> group_not;
             for (int b = -127; b < 128; b++) {
                 group_not.push_back(ComponentTestCase{ 
-                    {{"Data-1_in", 0}, {"Data-2_in", b}, {"Invert", 1}, {"Increment", 0}, {"Propagate", 0}},
-                    {{"Data_out", -b-1}} });
+                    {
+                        {"Data-1_in", TestNumber{0, false}}, 
+                        {"Data-2_in", TestNumber{b, true}}, 
+                        {"Invert", TestNumber{1, false}}, 
+                        {"Increment", TestNumber{0, false}}, 
+                        {"Propagate", TestNumber{0, false}}
+                    },
+                    {
+                        {"Data_out", TestNumber{-b-1, true}}
+                    } 
+                });
             }
             test_groups.insert(make_pair("Not", group_not));
 
@@ -210,8 +342,17 @@ namespace ComponentTests {
             for (int a = -127; a < 128; a++) {
                 for (int b = -127; b < 128; b++) {
                     group_xor.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}},
-                        {{"Data_out", a^b}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{0, false}}, 
+                            {"Increment", TestNumber{0, false}}, 
+                            {"Propagate", TestNumber{0, false}}
+                        },
+                        {
+                            {"Data_out", TestNumber{a^b, true}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("Xor", group_xor));
@@ -237,8 +378,17 @@ namespace ComponentTests {
             for (int a = -128; a < 128; a++) {
                 for (int b = -128; b < 128; b++) {
                     group_and.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}}, 
-                        {{"Data_out", a&b}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{0, false}}, 
+                            {"Increment", TestNumber{0, false}}, 
+                            {"Propagate", TestNumber{0, false}}
+                        }, 
+                        {
+                            {"Data_out", TestNumber{a&b, true}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("And", group_and));
@@ -264,8 +414,17 @@ namespace ComponentTests {
             for (int a = -128; a < 128; a++) {
                 for (int b = -128; b < 128; b++) {
                     group_or.push_back(ComponentTestCase{ 
-                        {{"Data-1_in", a}, {"Data-2_in", b}, {"Invert", 0}, {"Increment", 0}, {"Propagate", 0}}, 
-                        {{"Data_out", a|b}} });
+                        {
+                            {"Data-1_in", TestNumber{a, true}}, 
+                            {"Data-2_in", TestNumber{b, true}}, 
+                            {"Invert", TestNumber{0, false}}, 
+                            {"Increment", TestNumber{0, false}}, 
+                            {"Propagate", TestNumber{0, false}}
+                        }, 
+                        {
+                            {"Data_out", TestNumber{a|b, true}}
+                        } 
+                    });
                 }
             }
             test_groups.insert(make_pair("Or", group_or));
