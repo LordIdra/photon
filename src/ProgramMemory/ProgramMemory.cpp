9cee19311ade50d1d15fd760ce502e50f169f867
MetaMechanists <percy.baker.0@gmail.com>
2023-05-09 20:38:48 +0100

    Timings

--- src/ProgramMemory/ProgramMemory.cpp
+++ src/ProgramMemory/ProgramMemory.cpp
@@ -3,6 +3,7 @@
 #include "ComponentTest/ComponentTestCase.hpp"
 #include "Util/Util.hpp"
 #include "Util/Colors.hpp"
+#include "Util/Timer.hpp"
 #include <thread>
 #include <wiringPi.h>
 
@@ -15,6 +16,8 @@ namespace ProgramMemory {
         const GPIO::PinBlock block_data_in {12, 8, INPUT};
         const GPIO::PinBlock block_not_write_enable {20, 1, OUTPUT};
         const GPIO::PinBlock block_not_output_enable {21, 1, OUTPUT};
+
+        Timer timer;
 
         auto SetModeRead() -> void {
             GPIO::Set(block_not_output_enable, TestNumber{0});
@@ -29,23 +32,23 @@ namespace ProgramMemory {
         auto Write(const int address, const int value) -> void {
             GPIO::Set(block_address, TestNumber{address, false});
             GPIO::SetupPinBlock(block_data_out);
-            std::this_thread::sleep_for(std::chrono::microseconds(PROPAGATION_DELAY_MICROSECONDS));
+            std::this_thread::sleep_for(std::chrono::microseconds(WRITE_DELAY_MICROSECONDS));
             GPIO::Set(block_data_out, TestNumber{value, true});
-            std::this_thread::sleep_for(std::chrono::microseconds(PROPAGATION_DELAY_MICROSECONDS));
+            std::this_thread::sleep_for(std::chrono::microseconds(WRITE_DELAY_MICROSECONDS));
             SetModeWrite();
         }
 
         auto Read(const int address) -> unsigned int {
             SetModeRead();
             GPIO::SetupPinBlock(block_data_in);
-            std::this_thread::sleep_for(std::chrono::microseconds(PROPAGATION_DELAY_MICROSECONDS));
+            std::this_thread::sleep_for(std::chrono::microseconds(READ_DELAY_MICROSECONDS));
             GPIO::Set(block_address, TestNumber{address, false});
-            std::this_thread::sleep_for(std::chrono::microseconds(PROPAGATION_DELAY_MICROSECONDS));
+            std::this_thread::sleep_for(std::chrono::microseconds(READ_DELAY_MICROSECONDS));
             return GPIO::ReadInt(block_data_in, true);
         }
 
         void PrintErrors(const int errors) {
-            cout << WHITE << "Completed with ";
+            cout << WHITE << "Completed in " << CYAN << timer.GetMilliseconds() << WHITE << "ms with ";
             if (errors == 0) {
                 cout << GREEN << 0;
             } else {
@@ -56,6 +59,8 @@ namespace ProgramMemory {
     }
 
     auto Test() -> void {
+        timer.Start();
+
         GPIO::SetupWiringPi();
         GPIO::SetupPinBlock(block_address);
         GPIO::SetupPinBlock(block_not_write_enable);
@@ -66,8 +71,8 @@ namespace ProgramMemory {
 
         int errors = 0;
         
-        for (int address = 0; address < 8192; address++) {
-            for (int number = 0; number < 128; number++) {
+        for (int address = 0; address < 4096; address++) {
+            for (int number = -5; number < 5; number++) {
                 Write(address, number);
                 const unsigned int data = Read(address);
                 if (data != number) {
@@ -75,6 +80,8 @@ namespace ProgramMemory {
                 }
             }
         }
+
+        timer.Stop();
 
         PrintErrors(errors);
     }
